422adbfb8e26c7c7379ec3dd3d2ac8fb9588abfc
Kshitij Waghode <73355759+B2-80494@users.noreply.github.com>
2025-01-22 21:24:15 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -56,6 +56,11 @@ if ingredients_list:
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
 
+        
+        search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
+        st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
+
+        
         # New section to display smoothiefront nutrition information
         
         # This is the original line of code which the workshop is asking me to use.
